99e5d5e6b846a184825b3e184ca1ee707307b713
alanrehfeldt <alanrehfeldt@gmail.com>
2024-05-15 16:20:49 -0300

    criando camada de infraestrutura

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -5,19 +5,19 @@ import {
   Controller,
   HttpCode,
   Post,
-} from '@nestjs/common'
-import { PrismaService } from '@/prisma/prisma.service'
-import { hash } from 'bcryptjs'
-import { string, z } from 'zod'
-import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
+} from '@nestjs/common';
+import { PrismaService } from '@/infra/prisma/prisma.service';
+import { hash } from 'bcryptjs';
+import { string, z } from 'zod';
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe';
 
 const createAccountBodySchema = z.object({
   name: string(),
   email: string().email(),
   password: string(),
-})
+});
 
-type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
 
 @Controller('/accounts')
 export class CreateAccountController {
@@ -27,21 +27,21 @@ export class CreateAccountController {
   @HttpCode(201)
   @UsePipes(new ZodValidationPipe(createAccountBodySchema))
   async handle(@Body() body: CreateAccountBodySchema) {
-    const { name, email, password } = body
+    const { name, email, password } = body;
 
     const userWithSameEmail = await this.prisma.user.findUnique({
       where: {
         email,
       },
-    })
+    });
 
     if (userWithSameEmail) {
       throw new ConflictException(
-        'User with same e-mail address alredy exists.',
-      )
+        'User with same e-mail address alredy exists.'
+      );
     }
 
-    const hashedPassword = await hash(password, 8)
+    const hashedPassword = await hash(password, 8);
 
     await this.prisma.user.create({
       data: {
@@ -49,6 +49,6 @@ export class CreateAccountController {
         email,
         password: hashedPassword,
       },
-    })
+    });
   }
 }
